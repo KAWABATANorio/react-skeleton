1da3596bc9444d11ab92b11b3da2381e1a791f0c
KAWABATA Norio <kawabata@neoscorp.jp>
2025-04-03 16:25:11 +0900

    add strict

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,18 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
 
-export default tseslint.config(
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+    }
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,6 +23,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +31,18 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      ...prettier.configs.recommended.rules,
+      '@typescript-eslint/no-unused-expressions': 'off',
+      'no-undef': 'off',
+      indent: ['error', 2, { SwitchCase: 1, 'offsetTernaryExpressions': true }],
+      'no-multiple-empty-lines': ['error', { max: 3, maxEOF: 0, maxBOF: 0 }],
+      quotes: ['error', 'single', { 'allowTemplateLiterals': true }],
+      semi: ['error', 'never', { beforeStatementContinuationChars: 'never' }],
+      'semi-spacing': ['error', { after: true, before: false }],
+      'semi-style': ['error', 'first'],
+      'no-extra-semi': 'error',
+      'no-unexpected-multiline': 'error',
+      'no-unreachable': 'error',
     },
   },
-)
+]
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+export default {
+  "trailingComma": "es5",
+  "useTabs": false,
+  "tabWidth": 2,
+  "semi": false,
+  "singleQuote": true,
+  "printWidth": 150,
+  "arrowParens": "avoid"
+}
