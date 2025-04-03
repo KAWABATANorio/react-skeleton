052514f46c64c63050777d23c8329335344f8f60
KAWABATA Norio <kawabata@neoscorp.jp>
2025-04-03 16:59:00 +0900

    add MUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { CssBaseline } from '@mui/material'
+import './index.scss'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </StrictMode>
 )
